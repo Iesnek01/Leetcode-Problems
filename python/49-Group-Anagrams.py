765fcc408003e68b273a52b9c652a330289ee1fd
Yao Xiao <33704236+Iesnek01@users.noreply.github.com>
2022-08-10 10:32:02 -0400

    Update 49-Group-Anagrams.py

--- python/49-Group-Anagrams.py
+++ python/49-Group-Anagrams.py
@@ -1,10 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            ans[tuple(count)].append(s)
-        return ans.values()
+        dic = {}
+        groupby = {}
+        ans = []
+        
+        for i in range(len(strs)):
+            dic[i] = "".join(sorted(strs[i]))
+        for k,v in dic.items():
+            groupby[v] = [k] if v not in groupby.keys() else groupby[v] + [k]
+        
+        for value in groupby.values():
+            tmp = []
+            for i in value:
+                tmp.append(strs[i])
+            ans.append(tmp)
+        return ans
